25b26f2144a0570e84c81a92c2cade1c03374b83
ecspangler <oishiikani@gmail.com>
2019-10-14 18:18:25 -0400

    service update

--- credit-dispute-service/src/main/java/org/example/credit/dispute/service/CreditDisputeService.java
+++ credit-dispute-service/src/main/java/org/example/credit/dispute/service/CreditDisputeService.java
@@ -6,8 +6,7 @@ import io.vertx.core.json.Json;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
-import org.example.credit.dispute.producer.CreditDisputesProducer;
-import org.example.domain.dispute.Disputes;
+import io.vertx.ext.web.handler.StaticHandler;
 
 import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
@@ -15,6 +14,9 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.logging.Logger;
+
+import org.example.credit.dispute.producer.CreditDisputesProducer;
+import org.example.domain.dispute.Disputes;
 
 public class CreditDisputeService extends AbstractVerticle {
 
@@ -25,29 +27,37 @@ public class CreditDisputeService extends AbstractVerticle {
 		Router router = Router.router(vertx);
 		router.route().handler(BodyHandler.create());
 
-		router.post("/disputes-service/disputes").handler(this::createDisputes);
+		router.post("/payments-service/payments").handler(this::createPayments);
+		router.post("/transactions-service/queries/transactions").handler(this::getTransactionsByAccount);
+
+		router.get("/*").handler(StaticHandler.create());
 
 		vertx.createHttpServer().requestHandler(router::accept).listen(8080);
 		LOGGER.info("THE HTTP APPLICATION HAS STARTED");
+
 	}
 
-	private void createDisputes(RoutingContext routingContext) {
+	private void createPayments(RoutingContext routingContext) {
 		Disputes disputes = routingContext.getBodyAsJson().mapTo(Disputes.class);
-		LOGGER.info("Creating disputes: " + disputes);
+		LOGGER.info("Creating payments: " + disputes);
 
-		CreditDisputesProducer creditDisputesProducer = new CreditDisputesProducer();
-		disputes.getDisputes().forEach(dispute -> {
+		// CreditDisputesProducer debtorPaymentsProducer = new CreditDisputesProducer();
+		disputes.getDisputes().forEach(payment -> {
+
+			// Find the routing number and account number for the payee
+			// payment = PayeeAccountLookupBean.enrichPayeeAccountInformation(payment);
 
 			try {
-				// Dispute key generated based on timestamp for the reference example
+				// Payment key generated based on timestamp for the reference example
 				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS");
 				LocalDateTime now = LocalDateTime.now();
-				dispute.setDisputeId("DISPUTE" + formatter.format(now));
+				// payment.setPaymentId("KEYFORTHBANK" + formatter.format(now));
 
-				creditDisputesProducer.sendMessage(dispute.getDisputeId(), dispute);
+				// debtorPaymentsProducer.sendMessage(payment.getPaymentId(), payment);
+				// debitPaymentRepository.addPayment(new DebitPayment(payment));
 
 			} catch (Exception e) {
-				LOGGER.severe("Error publishing dispute to topic");
+				LOGGER.severe("Error publishing payment to topic");
 				LOGGER.severe(e.getMessage());
 				e.printStackTrace();
 			}
@@ -58,6 +68,50 @@ public class CreditDisputeService extends AbstractVerticle {
 		response.putHeader("Access-Control-Allow-Origin", "*");
 
 		response.end(Json.encode(disputes));
+	}
+
+	private void getTransactionsByAccount(RoutingContext routingContext) {
+		TransactionsRequest transactionsRequest = routingContext.getBodyAsJson().mapTo(TransactionsRequest.class);
+		LOGGER.info("Retrieving transactions for account: " + transactionsRequest.getAccountNumber());
+		// Transactions transactions = new Transactions();
+		Disputes disputes = new Disputes();
+
+		// call lookups
+		// List<DebitPayment> debitPayments =
+		// debitPaymentRepository.getPayments(transactionsRequest.getAccountNumber());
+		// List<CreditPayment> creditPayments = creditPaymentRepository
+		// .getPayments(transactionsRequest.getAccountNumber());
+
+//		debitPayments.forEach(debitPayment -> {
+//			Transaction transaction = new Transaction();
+//			transaction.setTransId(debitPayment.getPaymentId());
+//			transaction.setAmount(debitPayment.getAmount());
+//			transaction.setCreditDebitCode("DEBIT");
+//			transaction.setReceiverFirstName(debitPayment.getReceiverFirstName());
+//			transaction.setReceiverLastName(debitPayment.getReceiverLastName());
+//			transaction.setReceiverEmail(debitPayment.getReceiverEmail());
+//			transaction.setReceiverCellPhone(debitPayment.getReceiverCellPhone());
+//			transaction.setStatus(debitPayment.getStatus());
+//			transactions.getTransactions().add(transaction);
+//		});
+
+//		creditPayments.forEach(creditPayment -> {
+//			Transaction transaction = new Transaction();
+//			transaction.setTransId(creditPayment.getPaymentId());
+//			transaction.setAmount(creditPayment.getAmount());
+//			transaction.setCreditDebitCode("CREDIT");
+//			transaction.setReceiverFirstName(creditPayment.getReceiverFirstName());
+//			transaction.setReceiverLastName(creditPayment.getReceiverLastName());
+//			transaction.setReceiverEmail(creditPayment.getReceiverEmail());
+//			transaction.setReceiverCellPhone(creditPayment.getReceiverCellPhone());
+//			transaction.setStatus(creditPayment.getStatus());
+//			transactions.getTransactions().add(transaction);
+//		});
+
+		// LOGGER.info("Retrieved transactions: " + transactions.getTransactions());
+
+		routingContext.response().putHeader(CONTENT_TYPE, "application/json; charset=utf-8")
+				.putHeader("Access-Control-Allow-Origin", "*").end(Json.encodePrettily(disputes));
 	}
 
 }
