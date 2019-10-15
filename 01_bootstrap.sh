72a5045a027ac0e6436f42730594b7913958c54e
ecspangler <oishiikani@gmail.com>
2019-10-15 00:50:26 -0400

    process updates, consumer group correction

--- 01_bootstrap.sh
+++ 01_bootstrap.sh
@@ -121,3 +121,21 @@ oc new-app \
     --name=kafka-ui \
     landoop/kafka-topics-ui
 oc expose svc/kafka-ui
+
+
+
+# --- Install Jaeger
+oc create namespace observability
+oc create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/crds/jaegertracing_v1_jaeger_crd.yaml
+oc create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/service_account.yaml
+oc create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/role.yaml
+oc create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/role_binding.yaml
+oc create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/operator.yaml
+
+
+oc apply -f - <<EOF
+apiVersion: jaegertracing.io/v1
+kind: Jaeger
+metadata:
+  name: simplest
+EOF
