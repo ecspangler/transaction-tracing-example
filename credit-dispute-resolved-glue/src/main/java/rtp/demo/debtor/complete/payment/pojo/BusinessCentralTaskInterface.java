ba6deaab1c0441b260e103743084c958d062c0fa
ecspangler <oishiikani@gmail.com>
2019-10-15 06:15:01 -0400

    commit

---
+++ credit-dispute-resolved-glue/src/main/java/rtp/demo/debtor/complete/payment/pojo/BusinessCentralTaskInterface.java
@@ -0,0 +1,13 @@
+package rtp.demo.debtor.complete.payment.pojo;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+public interface BusinessCentralTaskInterface {
+
+	@PUT
+	@Path("/services/rest/server/containers/CreditDisputeProcessingEngine_1.0.0-SNAPSHOT/cases/instances/{caseId}/tasks/Credit%20Card%20Dispute%20Resolved")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void triggerAdhocTask(@PathParam("caseId") String caseId, String body);
+}
